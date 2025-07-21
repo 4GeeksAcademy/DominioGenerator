916637126acbd0ed51ea1a4553ff40d53526b2f0
LeonardoCalonico <dcalonico2@gmail.com>
2025-07-21 18:53:07 +0000

    actualización

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
 window.onload = function() {
-  //write your code here
+let pronoun = ['el', 'nuestro', 'su', 'mi', 'tu'];
+let adj = ['gran', 'encantador', 'magistral', 'sospechoso', 'gigantesco'];
+let noun = ['juguete', 'unicornio', 'colisionadordeadrones', 'coheteespacial', 'naufragio',];
+let dom = ['.com', '.net', 'org', 'es', 'gov', 'xxx', '.mx'];
+
+const generador = () => {
+
+
+    for (let ip = 0; ip < pronoun.length; ip++) {
+        const pronombre = pronoun[ip];
+
+        for (let ia = 0; ia < adj.length; ia++) {
+            const adjetivo = adj[ia];
+
+            for (let ino = 0; ino < noun.length; ino++) {
+                const sustantivo = noun[ino];
+
+                for (let id = 0; id < dom.length; id++) {
+                    const dominio = dom[id];
+
+                    let resultado = pronombre + adjetivo + sustantivo + dominio;
+                    console.log(resultado);
+
+                    
+
+                }
+            }
+
+        }
+
+    }
+}
+generador();
   console.log("Hello Rigo from the console!");
 };
+
